8074a5d0cdc11c589d273a095ffffc52e86a5e9c
Jean Duarte <jeanduarte322@gmail.com>
2022-01-20 11:12:44 -0300

    main

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 #Alterar os paths de acordo com as futuras pastas
-BACKUPFONTE = r'C:\exemplo...'
+BACKUPFONTE = r'C:\Users\jeand\exemplo...'
 BACKUPDESTINO = r'G:\Meu Drive\exemplo...'
 
 MINUTOS_ENTRE_CHECKAGENS = 30 # em minutos
