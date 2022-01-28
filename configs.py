429b4e41772700c8eb2e384d54d006d46b88d3fe
Jean Duarte <jeanduarte322@gmail.com>
2022-01-28 08:57:04 -0300

    Adicionando 2 variaveis

--- configs.py
+++ configs.py
@@ -3,3 +3,5 @@ BACKUP_FONTE = r'C:\Users\jeand\Documents\ProjetoPython\BackupFonteSankhya'
 BACKUP_DESTINO = r'G:\Meu Drive\BackupSankhya'
 #Extensao do arquivo que deseja copiar
 EXTENSAO = '.txt'
+INTERVALO_1_BACKUP = 9
+INTERVALO_2_BACKUP = 21
